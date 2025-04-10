3aaaa9ff2d893f714184f653c6271efc61ab2ad8
David Jones <davidjones@sportronics.com.au>
2025-04-10 18:11:56 +1000

    Maui app can launch in Andriod Phone. Can stitch in Windows-Not Mobile

--- StitchInTime/MainPage.xaml.cs
+++ StitchInTime/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using System.Threading.Tasks;
 
 
 
@@ -147,7 +148,20 @@ namespace StitchInTime
                 MainThread.InvokeOnMainThreadAsync(() => viewModel.IsBussy = true);
             }
             CounterBtn.Text = $"Stitching";
-            StitchUp(videoPath, outputPath, startTimeSeconds);
+            //StitchUp(videoPath, outputPath, startTimeSeconds);
+            asyncStitchUp(videoPath, outputPath, startTimeSeconds).ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    // Handle any exceptions that occurred during the async operation
+                    System.Diagnostics.Debug.WriteLine($"Stitchup Run Error: {t.Exception?.Message}");
+                }
+                else
+                {
+                    // Continue with the rest of your code after the async operation completes
+                    System.Diagnostics.Debug.WriteLine("Async Stitchup operation completed successfully.");
+                }
+            });
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
 
@@ -172,14 +186,57 @@ namespace StitchInTime
             }
         }
 
-        private void StitchUp(string videoPath, string outputPath, int startTimeSeconds)
+        private void GetStitchUp(object sender, EventArgs e)
+        {
+            asyncGetStitchUp().ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    // Handle any exceptions that occurred during the async operation
+                    System.Diagnostics.Debug.WriteLine($"GetStitchUp Files Error: {t.Exception?.Message}");
+                }
+                else
+                {
+                    // Continue with the rest of your code after the async operation completes
+                    System.Diagnostics.Debug.WriteLine("Async operation GetStitchUp Files completed successfully.");
+                }
+            });
+        }
+
+        private async Task asyncGetStitchUp()
         {
 
-
-            if (!File.Exists(videoPath))
+            var result = await PickAndShow(true);
+            if (result == null)
             {
                 return;
             }
+            string? videoP = result?.FullPath;
+            if (videoP == null)
+            {
+                return;
+            }
+            videoPath = videoP;
+            Entry vid = (Entry)FindByName("Source");
+            vid.Text = videoPath;
+            result = await PickAndShow(false);
+            if (result == null)
+            {
+                return;
+            }
+            string? oPath = result?.FullPath;
+            if (oPath == null)
+            {
+                return;
+            }
+            outputPath = oPath;
+            Entry stitch = (Entry)FindByName("Stitch");
+            stitch.Text = outputPath;
+        }
+
+        private async Task asyncStitchUp(string videoPath, string outputPath, int startTimeSeconds)
+        {
+
 
             // Run the stitching process in a background thread
             BackgroundWorker worker = new BackgroundWorker();
@@ -212,5 +269,49 @@ namespace StitchInTime
 
             worker.RunWorkerAsync();
         }
+
+        public async Task<FileResult> PickAndShow(bool isvideo)
+        {
+            PickOptions options;
+            if(isvideo)
+            {
+                options = new PickOptions
+                {
+                    PickerTitle = "Please select a video file",
+                    FileTypes = FilePickerFileType.Videos
+                };
+            }
+            else
+            {
+                options = new PickOptions
+                {
+                    PickerTitle = "Please select an image file",
+                    FileTypes = FilePickerFileType.Images
+                };
+            }
+            try
+            {
+                var result = await FilePicker.Default.PickAsync(options);
+                if (result != null)
+                {
+                    if (/*result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||*/
+                        result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                    {
+                        //using var stream = await result.OpenReadAsync();
+                        //var image = ImageSource.FromStream(() => stream);
+                    }
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                // The user canceled or something went wrong
+            }
+
+            return null;
+        }
+
+
     }
 }
