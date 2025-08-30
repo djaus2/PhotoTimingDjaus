ab289b5b5f9a483fc56386b70623858c8d61a959
David Jones <davidjones@sportronics.com.au>
2025-08-30 16:33:01 +1000

    On Download Dialog Mwenu File-Done to return

--- AthStitcher/GetVideoPage.xaml.cs
+++ AthStitcher/GetVideoPage.xaml.cs
@@ -17,6 +17,9 @@ namespace AthStitcherGUI
         public GetVideoPage()
         {
             InitializeComponent();
+            this.WindowStyle = WindowStyle.None;
+            this.ResizeMode = ResizeMode.NoResize;
+
         }
 
         protected override void OnContentRendered(EventArgs e)
